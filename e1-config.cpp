a1fc85a4059fecfadf14adbb998ce6c6ba3173e0
Erik Frojdh <erik.frojdh@gmail.com>
2020-03-17 11:28:33 +0100

    e1

---
+++ e1-config.cpp
@@ -0,0 +1,20 @@
+
+
+#include "Detector.h"
+#include <iostream>
+
+int main(int argc, char *argv[]){
+
+    if (argc != 2){
+        std::cout << "Wrong number of arguments provided, exiting.\n";
+        return 0;
+    }
+
+    sls::Detector det;
+    det.loadConfig(argv[1]);
+    
+    auto type = sls::ToString(det.getDetectorType().squash());
+    auto nmod = det.size();
+    std::cout << type << " detector with " << nmod << " modules configured\n";
+
+}
